358a2a122de4fa6ca8b9e20f2165dd482535de08
Ana Lucia Ramirez <alramirez@lacomercial.gt>
2026-05-22 11:00:00 -0600

    crea modulo de descuentos

---
+++ descuentos.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Modulo de descuentos de La Comercial
+
+DESCUENTO_CLIENTE_FRECUENTE = 0.05
+
+def descuento_cliente_frecuente(subtotal):
+    return subtotal * DESCUENTO_CLIENTE_FRECUENTE
